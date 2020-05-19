daa79caf7c2cb346928cf0d7d82af067e26382f3
Eugene Sobol <esobol@insoftgroup.com>
2020-05-19 17:02:30 +0300

    Initial Commit

---
+++ BicycleMaintenance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BicycleMaintenance
+//
+//  Created by Евгений Соболь on 5/19/20.
+//  Copyright © 2020 Esobol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
